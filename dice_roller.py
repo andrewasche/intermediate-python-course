38f85967fc343cacaa1eadd57c417fc3907111a9
Andrew Asche <asche069@gmail.com>
2021-10-02 15:12:34 -0500

    now rolling two dices

--- dice_roller.py
+++ dice_roller.py
@@ -3,12 +3,18 @@ import random
 def main():
   print('You rolled a die')
   rolls = 3
-  sum = 0
-  for i range(0, rolls):
+  dice_sum = 0
+  for i in range(0, rolls):
 	roll = random.randint(1,6)
 	dice_sum += roll
-	print(f'You rolled a {roll}')
-	print(f'You have rolled a total of {dice_sum}')
+
+	if roll == 1:
+	  print(f'You rolled a {roll}! Critical Fail')
+	elif roll == 6:
+	  print(f'You rolled a {roll}! Critical Success!')
+	else:
+	  print(f'You rolled a {roll}')
+  print(f'You have rolled a total of {dice_sum}')
 
 if __name__== "__main__":
   main()
